09cd3d758c522358aa4dbf0693ce65579f222881
Waldenth <waldenth@outlook.com>
2021-03-19 13:36:49 +0800

    add problem 206 another solution

--- #206 反转链表.java
+++ #206 反转链表.java
@@ -21,3 +21,47 @@ class Solution {
         return preNode;
     }
 }
+
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head==null||head.next==null||left>=right)
+            return head;
+
+        ListNode exHead=new ListNode(-1,head);
+        ListNode leftNode=exHead,rightNode=exHead,preNode=exHead,succNode=exHead;
+        ListNode tmpNode=exHead;
+
+        int i=0;
+        while(i<right){
+            if(i==left-1){
+                preNode=tmpNode;
+                leftNode=tmpNode.next;
+            }
+            if(i==right-1){
+                rightNode=tmpNode.next;
+                succNode=rightNode.next;
+            }
+            
+            tmpNode=tmpNode.next;
+            i++;
+        }        
+        
+        reverseList(leftNode,succNode,leftNode.next);
+        
+        preNode.next=rightNode;
+        leftNode.next=succNode;
+
+        return exHead.next;
+    }
+    private ListNode reverseList(ListNode listPreNode,ListNode listSuccNode,ListNode curNode){
+        ListNode curPreNode=listPreNode;
+        ListNode curSuccNode=null;
+        while(curNode!=listSuccNode){
+            curSuccNode=curNode.next;
+            curNode.next=curPreNode;
+            curPreNode=curNode;
+            curNode=curSuccNode;
+        }
+        return curPreNode;
+    }
+}
