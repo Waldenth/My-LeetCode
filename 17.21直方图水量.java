947636cbf8602e1f5542c81080586ea3260d4ee7
Waldenth <waldenth@outlook.com>
2021-04-02 22:40:39 +0800

    fuck

---
+++ 17.21直方图水量.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int trap(int[] height) {
+        if(height.length<=2)
+            return 0;
+        
+        Deque<Integer>stack=new ArrayDeque<>();
+        
+        int ans=0;
+
+        for(int i=0;i<height.length;i++){
+            //System.out.println(stack.peekFirst()+" "+i);
+            if(stack.size()==0){
+                stack.addFirst(i);
+            }else{
+                if(height[stack.peekFirst()]>=height[i]){
+                    stack.addFirst(i);
+                }else{
+                    while(stack.size()>0){
+                        int bottom=height[stack.pop()];
+                        //System.out.println("bottom="+bottom);
+                        if(stack.size()>0){
+                            if(height[stack.peekFirst()]==bottom){
+                                stack.pop();
+                            }
+                            if(stack.size()>0){
+                                //System.out.println("left="+height[stack.peekFirst()]+" right="+height[i]+" i="+i);
+                                ans+=(Math.min(height[stack.peekFirst()],height[i])-bottom)*(i-stack.peekFirst()-1);
+                                //System.out.println("ans="+ans);
+                            }
+                            if(stack.peekFirst()!=null&&height[i]>height[stack.peekFirst()]){;
+                                continue;
+                            }else{
+                                break;
+                            }
+                        }else{
+                            break;
+                        }
+                    }
+                    stack.addFirst(i);
+                }
+            }
+        }
+        return ans;
+    }
+}
