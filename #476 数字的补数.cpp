095dc3051781a66e56d9678ea994f6882cb9bac1
Waldenth <waldenth@outlook.com>
2021-10-18 20:07:37 +0800

    add pro 476

---
+++ #476 数字的补数.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int highBit=0; //0~30 
+        for(int i=1;i<=30;i++){
+            if(num>=(1<<i)){
+                highBit=i;
+            }else
+                break;
+        }
+        int mask = (highBit == 30 ? 0x7fffffff : (1 << (highBit + 1)) - 1);
+        return num ^ mask;
+    }
+};
